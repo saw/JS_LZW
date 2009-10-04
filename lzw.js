35e504bde06da19904d8623016142651567bbcbe
Stephen Woods <saw@yahoo-inc.com>
2009-10-03 17:07:04 -0700

    it all works!

--- lzw.js
+++ lzw.js
@@ -40,7 +40,7 @@
           for(var c in s ){
               outStr.push(s[c]);
           }
-          
+
           return outStr.join('');
         };
         
@@ -60,29 +60,32 @@
             buffer = '',
             outStr = [];
            
-            outStr.push(str[0]);
+           // outStr.push(str[0]);
             var first_code = str[0].charCodeAt(0);
             var len = str.length;
             var counter = 255;
-            for (var i=1; i < len; i++) {
+            var character = '';
+            for (var i=0; i < len; i++) {
                 var next_code = str[i].charCodeAt(0);
                 
-                if(table[next_code]){
-                    buffer = table[next_code]
-                
-                    table[++counter] = table[first_code] +
-                                       table[next_code];
-                    first_code = next_code;
+                if(!table[next_code]){
+                    buffer = table[first_code];
+                    buffer = buffer + character;
                 }else{
-                    console.log('else');
-                    buffer = table[first_code].charCodeAt(0);
-                    buffer = buffer + next_code;
-                    buffer = String.fromCharCode(buffer);
-                    console.log(buffer);
-;                }
+                    buffer = table[next_code];
+ 
+                }
                 
                 outStr.push(buffer);
+                character = buffer[0];
+                table[++counter] = table[first_code] +
+                                   character;
+
+                first_code = next_code;
+            
+                
             };
+            
             return outStr.join('');
  
         }
